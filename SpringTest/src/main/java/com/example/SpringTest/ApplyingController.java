a62615125e699e04b92dbb2dfef2e13fa06e96e7
Stas <leko@mail.ru>
2019-10-30 01:12:11 +0500

    just commit

---
+++ SpringTest/src/main/java/com/example/SpringTest/ApplyingController.java
@@ -0,0 +1,4 @@
+package com.example.SpringTest;
+
+public class ApplyingController {
+}
--- SpringTest/target/classes/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'React'
-
-function HelloWorld {
-    return (
-        <div>
-            Hello World
-        </div>
-    )
-}
