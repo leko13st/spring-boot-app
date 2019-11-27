7c3b0451f4dcaccacb2ff839df621520ae34501d
leko13st <lekomstas13@mail.ru>
2019-11-27 17:08:08 +0500

    added sort by js

--- SpringTest/src/main/resources/static/js/myJS.js
+++ SpringTest/src/main/resources/static/js/myJS.js
@@ -16,3 +16,18 @@ function sum(){
 		result.innerHTML = Number(price.innerHTML);
 	}
 }
+
+
+function inputChange(){
+    var filter = document.getElementById('input-filter');
+    let str = filter.value.toLowerCase();
+    let tables = document.querySelectorAll('.table');
+
+    for (let i = 0; i < tables.length; i++){
+        var car = document.querySelectorAll('.car')[i].innerText.toLowerCase();
+        if (car.indexOf(str) == -1)
+            tables[i].style.display = "none";
+        else
+            tables[i].style.display = "table";
+    }
+}
